3c52b5d4b0c941c2dc7e1fe5493dc13dc752b386
Karen Roh <karenroh@hotmail.com>
2021-07-12 09:51:04 -0400

    implemented the apollo server and applied it to the express server as middleware

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// * `server.js`: Implement the Apollo Server and apply it to the Express server as middleware.
+
+const express = require("express");
+
+//implementing apollo server
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//creating an instance of an Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+//applying apollo server to the Express server as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
