38c701458fae8397c65f2505a03640ea8070a693
Karen Roh <karenroh@hotmail.com>
2021-07-12 13:26:54 -0400

    installed apollo/client dependency

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,49 @@
 // 	* `resolvers.js`: Define the query and mutation functionality to work with the Mongoose models.
 // **Hint**: Use the functionality in the `user-controller.js` as a guide.
-const { Book, User } = require("../models");
 
-const resolvers = {};
+const { AuthenticationError } = require("apollo-server-express");
+// import user model
+const { User } = require("../models");
+// import sign token function from auth
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async () => {
+      return User.find();
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        throw new AuthenticationError("Something is wrong!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    //how to take args as input?
+    saveBook: async () => {},
+  },
+};
 
 module.exports = resolvers;
