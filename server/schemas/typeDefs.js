8a5af866b2f4067fa39e8877ca646eb907a47cc1
Karen Roh <karenroh@hotmail.com>
2021-07-12 12:06:07 -0400

    completed layout of typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,63 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [String]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input saveBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: ID
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+
+    saveBook(input: saveBookInput): User
+
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
+
 // * `typeDefs.js`: Define the necessary `Query` and `Mutation` types:
 // * `Query` type:
 //     * `me`: Which returns a `User` type.
+
 // * `Mutation` type:
 //     * `login`: Accepts an email and password as parameters; returns an `Auth` type.
 //     * `addUser`: Accepts a username, email, and password as parameters; returns an `Auth` type.
 //     * `saveBook`: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a `User` type. (Look into creating what's known as an `input` type to handle all of these parameters!)
 //     * `removeBook`: Accepts a book's `bookId` as a parameter; returns a `User` type.
+
 // * `User` type:
 //     * `_id`
 //     * `username`
@@ -22,30 +74,3 @@
 // * `Auth` type:
 //     * `token`
 //     * `user` (References the `User` type.)
-
-const { gql } = require("apollo-server-express");
-
-const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [String]
-  }
-
-  type Book {
-      bookId: ID
-      authors: [String]
-      description: String
-      title: String
-      image: String
-      link: String
-  }
-
-  type Query {
-      me: User
-  }
-`;
-
-module.exports = typeDefs;
