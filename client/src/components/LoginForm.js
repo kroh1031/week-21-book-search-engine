60e70ba40eb56fc518e6025f31a6aa0f196e8aaa
Karen Roh <karenroh@hotmail.com>
2021-07-15 10:09:20 -0400

    replaced the loginUser() functionality from the API file with the LOGIN_USER mutation functionality in LoginForm component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -14,6 +14,8 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -30,15 +32,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({ variables: { ...userFormData } });
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error("something went wrong!");
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
